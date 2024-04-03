fbb03836b1ba6bc240dd4c11da856fc2786b0ff2
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-04-02 22:56:53 -0400

    Add "hover" event for tiles to adjust element class

--- site/script.js
+++ site/script.js
@@ -7,6 +7,8 @@ for (let i = 0; i < Math.pow(sideLength, 2); i++) {
 	let tile = document.createElement("div");
 	tile.className = "tile";
 	tile.style.setProperty("flex-basis", tileBasis + '%');
+	tile.addEventListener("mouseover", (event) => {event.target.classList.add("active", "activated");});
+	tile.addEventListener("mouseout", (event) => {event.target.classList.remove("active");});
 
 	grid.appendChild(tile);
 }
