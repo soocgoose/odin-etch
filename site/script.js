aad45fdca28eb4f76fc324b26dd0c535856ddbaa
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-04-02 22:08:25 -0400

    Use variables to describe the grid size

--- site/script.js
+++ site/script.js
@@ -1,6 +1,9 @@
 let grid = document.querySelector('#grid');
 
-for (let i = 0; i < 256; i++) {
+const sideLength = 16;
+const tileBasis = 100 / sideLength;
+
+for (let i = 0; i < Math.pow(sideLength, 2); i++) {
 	let tile = document.createElement("div");
 	tile.className = "tile";
 
