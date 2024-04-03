c91329e2a883037f6068147ddc0c34fccbd7e4d1
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-04-03 09:03:38 -0400

    Change events from mouse to pointer to support touch

--- site/script.js
+++ site/script.js
@@ -7,8 +7,10 @@ for (let i = 0; i < Math.pow(sideLength, 2); i++) {
 	let tile = document.createElement("div");
 	tile.className = "tile";
 	tile.style.setProperty("flex-basis", tileBasis + '%');
-	tile.addEventListener("mouseover", (event) => {event.target.classList.add("active", "activated");});
-	tile.addEventListener("mouseout", (event) => {event.target.classList.remove("active");});
+
+	tile.addEventListener("pointerenter", (event) => {event.target.classList.add("active", "activated");});
+	tile.addEventListener("pointerleave", (event) => {event.target.classList.remove("active");});
+	tile.addEventListener("pointerdown", (event) => {event.target.releasePointerCapture(event.pointerId);});
 
 	grid.appendChild(tile);
 }
