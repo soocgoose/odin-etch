ad21fb8419517eebd6824567e75588ef1febd9a3
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-04-03 14:40:35 -0400

    Encapsulate current behaviour in a function

--- site/script.js
+++ site/script.js
@@ -1,17 +1,21 @@
 let grid = document.querySelector('#grid');
 
-const sideLength = 16;
-const tileBasis = 100 / sideLength;
 
-for (let i = 0; i < Math.pow(sideLength, 2); i++) {
-	let tile = document.createElement("div");
-	tile.className = "tile";
-	tile.style.setProperty("flex-basis", tileBasis + '%');
+function createTileGrid(sideLength = 16) {
+	const tileBasis = 100 / sideLength;
 
-	tile.addEventListener("pointerenter", (event) => {event.target.classList.add("active", "activated");});
-	tile.addEventListener("pointerleave", (event) => {event.target.classList.remove("active");});
-	tile.addEventListener("pointerdown", (event) => {event.target.releasePointerCapture(event.pointerId);});
+	for (let i = 0; i < Math.pow(sideLength, 2); i++) {
+		let tile = document.createElement("div");
+		tile.className = "tile";
+		tile.style.setProperty("flex-basis", tileBasis + '%');
 
-	grid.appendChild(tile);
+		tile.addEventListener("pointerenter", (event) => {event.target.classList.add("active", "activated");});
+		tile.addEventListener("pointerleave", (event) => {event.target.classList.remove("active");});
+		tile.addEventListener("pointerdown", (event) => {event.target.releasePointerCapture(event.pointerId);});
+
+		grid.appendChild(tile);
+	}
 }
+
+createTileGrid();
 
